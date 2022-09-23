94116387a01c6a8f25bde2a8389d5d83dc2938d7
ezuykow <zuy1996@gmail.com>
2022-09-23 16:23:23 +0300

    Remove: web.xml
    Added:
    SpringConfig
    DispatcherServletInitializer

---
+++ src/main/java/ru/ezuykow/app/config/DispatcherServletInitializer.java
@@ -0,0 +1,19 @@
+package ru.ezuykow.app.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/ru/ezuykow/app/controllers/SecondController.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+
+    @GetMapping("/exit")
+    public String exitPage() {
+        return "second/exit";
+    }
+}
