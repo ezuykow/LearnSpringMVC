c795f06ac69a7d31487a6bf3080da185d034f13f
ezuykow <zuy1996@gmail.com>
2022-10-02 09:57:55 +0300

    watch README

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
-#LearnSpringMVC (Branch #5._upgrade_CRUD)
+#LearnSpringMVC (Branch #7._upgrade_CRUD(+DB))
 
-OLD:<br/>
-        Simple CRUD-app without DB (using ArrayList); <br/>
-        Added a functional to create new person;
-        Added a method-filter in DispatcherServletInitializer; <br/>
-        Added a button "Add new person" in index.html; <br/>
-        Added a functional to edit person:
-        Added a functional to delete person:
-<hr/>
 NEW:<br/>
-        Added a Hibernate-validator dependency in pom
-        Added a fields (email, age) to Person model<br/>
-        Added an age to index.html<br/>
-        Added an age and email to show.html<br/>
-        Update meth "update" in PeopleDAO;
-        Added fields in new.html<br/>
-        Added fields in edit.html<br/>
-        Added a valid-annotations to fields<br/>
-        Added a validation to controller<br/>
-        Added an error blocks to new.html<br/>
-        Added an error blocks to edit.html
-
+        Added a "PostgreSQL JDBC Driver" dependency<br/>  
+        Plug DB<br/>
+        Remove List from DAO<br/>
+        Rewrite meth index() in DAO (using JDBC)<br/>
+        Rewrite meth add(Person p) in DAO (using JDBC)<br/>
         
--- src/main/java/ru/ezuykow/app/dao/PeopleDAO.java
+++ src/main/java/ru/ezuykow/app/dao/PeopleDAO.java
@@ -3,37 +3,76 @@ package ru.ezuykow.app.dao;
 import org.springframework.stereotype.Component;
 import ru.ezuykow.app.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PeopleDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String DB_USERNAME = "ezuykow";
+    private static final String DB_PASSWORD = "579940974";
 
-    {
-        people = new ArrayList<>();
+    private static Connection connection;
 
-        people.add(new Person(++PEOPLE_COUNT, "Bob", "bob@gmail.com", 23));
-        people.add(new Person(++PEOPLE_COUNT, "Mary", "mary@gmail.com", 32));
-        people.add(new Person(++PEOPLE_COUNT, "Kate", "kate@gmail.com", 41));
-        people.add(new Person(++PEOPLE_COUNT, "Ivan", "ivan@gmail.com", 16));
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            final String SQL = "SELECT * FROM person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                var p = new Person();
+
+                p.setId(resultSet.getInt("id"));
+                p.setName(resultSet.getString("name"));
+                p.setEmail(resultSet.getString("email"));
+                p.setAge(resultSet.getInt("age"));
+
+                people.add(p);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return people;
     }
 
     public Person show(int id) {
-        return people.stream()
-                .filter(person -> person.getId() == id)
-                .findAny()
-                .orElse(null);
+        return null;
     }
 
-    public void add(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+    public void add(Person p) {
+        try {
+            Statement statement = connection.createStatement();
+            final String SQL = "INSERT INTO person VALUES (" +
+                    p.getId() +
+                    ", '" + p.getName() +
+                    "' , '" + p.getEmail() +
+                    "' , " + p.getAge() +
+                    ");";
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void update(int id, Person updatedPerson) {
@@ -44,6 +83,5 @@ public class PeopleDAO {
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
     }
 }
