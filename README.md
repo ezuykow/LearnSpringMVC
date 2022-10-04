c50588856ac3b23ce863aa9df98d4a6f938ece5d
ezuykow <zuy1996@gmail.com>
2022-10-04 15:40:06 +0300

    watch README

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-#LearnSpringMVC (Branch #9._upgrade_CRUD_(+JdbcTemplate))
-
-        Rewrite meth index() in DAO (using JDBC)<br/>
-        Rewrite meth add(Person p) in DAO (using PreparedStatement)<br/>
-        Rewrite meth show(int id) in DAO (using PreparedStatement)<br/>
-        Rewrite meth update(int id, Person updatedPerson) in DAO (using PreparedStatement)<br/>
-        Rewrite meth delete(int id) in DAO (using PreparedStatement)<br/>
+#LearnSpringMVC (Branch #10._upgrade_CRUD_(+properties))
 
 NEW:<br/>
-        Added Spring-JDBC dependency<br/>
-        Added beans DataSource & JdbcTemplate to SpringConfig<br/>
-        Added jdbcTemplate to PeopleDAO<br/>
-        Rewrite index() using jdbcTemplate<br/>
-        Rewrite show() using jdbcTemplate<br/>
-        Rewrite update() using jdbcTemplate<br/>
-        Rewrite add() using jdbcTemplate<br/>
-        Rewrite delete() using jdbcTemplate<br/>
+        Added resource "database.properties" (Added to .gitignore)<br/>
+        Added resource "database.properties.origin"<br/>
+        Added @PropertySource in SpringConfig<br/>
+        Added environment in SpringConfig<br/>
+        Rewrite Bean dataSource();
--- src/main/java/ru/ezuykow/app/config/SpringConfig.java
+++ src/main/java/ru/ezuykow/app/config/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,17 +17,22 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @ComponentScan("ru.ezuykow.app")
 @EnableWebMvc
+@PropertySource("classpath:database.properties")
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
 
+    private final Environment environment;
+
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -56,10 +63,10 @@ public class SpringConfig implements WebMvcConfigurer {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
-        dataSource.setUsername("ezuykow");
-        dataSource.setPassword("579940974");
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("db_driver")));
+        dataSource.setUrl(environment.getProperty("db_url"));
+        dataSource.setUsername(environment.getProperty("db_username"));
+        dataSource.setPassword(environment.getProperty("db_password"));
 
         return dataSource;
     }
